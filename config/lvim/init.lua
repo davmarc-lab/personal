75583517630c869519989b21b1d0e598c2bf58b0
Davide Marchetti <davide.marchetti6@studio.unibo.it>
2026-04-11 16:38:40 +0200

    latex nvim

---
+++ config/lvim/init.lua
@@ -0,0 +1,9 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+require("config.settings")
+require("config.keybinds")
+
+require("config.lazy")
+
+require("config.colorscheme")
---
+++ config/lvim/lua/plugins/core/nvim-latex.lua
@@ -0,0 +1,11 @@
+return {
+    "lervag/vimtex",
+    lazy = false, -- we don't want to lazy load VimTeX
+    init = function()
+        -- VimTeX configuration goes here, e.g.
+        vim.g.vimtex_view_method = "zathura"
+        vim.g.vimtex_compiler_latexmk = {
+            out_dir = "build",
+        }
+    end,
+}
