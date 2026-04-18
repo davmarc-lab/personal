464175360e9c6225683fc760434d7d22f1426929
Davide Marchetti <davide.marchetti6@studio.unibo.it>
2026-04-18 16:59:43 +0200

    lvim: fixed typo

--- config/lvim/lua/plugins/lsp/lsp-standard.lua
+++ config/lvim/lua/plugins/lsp/lsp-standard.lua
@@ -6,10 +6,52 @@ return {
             "williamboman/mason-lspconfig.nvim",
             "WhoIsSethDaniel/mason-tool-installer.nvim",
             { "folke/lazydev.nvim", opts = {} },
+            "saghen/blink.cmp",
         },
         config = function()
+            local mason = require("mason")
+            local mason_lspconfig = require("mason-lspconfig")
+
+            mason.setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗",
+                    },
+                },
+            })
+
+            mason_lspconfig.setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "stylua",
+                    "ltex_plus",
+                    "texlab"
+                },
+                automatic_enable = true
+            })
+
+            vim.lsp.config("ltex_plus", {
+                filetypes = {
+                    "tex",
+                    "bib",
+                    "markdown"
+                },
+                settings = {
+                    ltex = {
+                        "tex",
+                        "bib",
+                        "markdown"
+                    }
+                }
+            })
+
+            vim.lsp.config("*", {
+                capabilities = require("blink.cmp").get_lsp_capabilities(),
+            })
+
             vim.api.nvim_create_autocmd("LspAttach", {
-                group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
                 callback = function(event)
                     local map = function(keys, func, desc)
                         vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
@@ -31,11 +73,8 @@ return {
                     map("K", vim.lsp.buf.hover, "Hover Documentation")
                     map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
-                    --    See `:help CursorHold` for information about when this is executed
-                    --
-                    -- When you move your cursor, the highlights will be cleared (the second autocommand).
                     local client = vim.lsp.get_client_by_id(event.data.client_id)
-                    if client and client.server_capabilities.documentHighlightProvider then
+                    if client and client:supports_method("textDocument/documentHighlight", event.buf) then
                         vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
                             buffer = event.buf,
                             callback = vim.lsp.buf.document_highlight,
@@ -45,68 +84,45 @@ return {
                             buffer = event.buf,
                             callback = vim.lsp.buf.clear_references,
                         })
+
+                        vim.api.nvim_create_autocmd("LspDetach", {
+                            group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+                            callback = function(event2)
+                                vim.lsp.buf.clear_references()
+                                vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+                            end,
+                        })
                     end
                 end,
             })
-
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities = vim.tbl_deep_extend("force", capabilities, require("blink.cmp").get_lsp_capabilities())
-
-            local servers = {
-                lua_ls = {
-                    settings = {
-                        Lua = {
-                            completion = {
-                                callSnippet = "Replace",
-                            },
-                        },
-                    },
-                },
-            }
-
-            require("mason").setup()
-
-            local ensure_installed = vim.tbl_keys(servers or {})
-            vim.list_extend(ensure_installed, {
-                "stylua",
-            })
-            require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-            require("mason-lspconfig").setup({
-                ensure_installed = {},
-                automatic_installation = {},
-                handlers = {
-                    function(server_name)
-                        local server = servers[server_name] or {}
-                        server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-                        vim.lsp.config(server_name, server)
-                        vim.lsp.enable(server_name)
-                    end,
-                },
-            })
         end,
     },
-    {
+    { -- Autoformat
         "stevearc/conform.nvim",
-        lazy = false,
+        event = { "BufWritePre" },
+        cmd = { "ConformInfo" },
         keys = {
             {
                 "<leader>f",
                 function()
-                    require("conform").format({ async = true, lsp_fallback = true })
+                    require("conform").format({ async = true, lsp_format = "fallback" })
                 end,
                 mode = "",
                 desc = "[F]ormat buffer",
             },
         },
+        ---@module 'conform'
+        ---@type conform.setupOpts
         opts = {
             notify_on_error = false,
-            format_on_save = function(_) end,
+            format_on_save = function(_)
+                return nil
+            end,
         },
     },
     { -- Autocompletion
         "saghen/blink.cmp",
-        event = "InsertEnter",
+        event = "VimEnter",
         version = "1.*",
         dependencies = {
             -- Snippet Engine
@@ -164,185 +180,4 @@ return {
             signature = { enabled = true },
         },
     },
-    -- {
-    --     "neovim/nvim-lspconfig",
-    --     dependencies = {
-    --         "williamboman/mason.nvim",
-    --         "williamboman/mason-lspconfig.nvim",
-    --         "WhoIsSethDaniel/mason-tool-installer.nvim",
-    --         { "folke/lazydev.nvim", opts = {} },
-    --         "saghen/blink.cmp",
-    --     },
-    --     config = function()
-    --         local mason = require("mason")
-    --         local mason_lspconfig = require("mason-lspconfig")
-    --
-    --         mason.setup({
-    --             ui = {
-    --                 icons = {
-    --                     package_installed = "✓",
-    --                     package_pending = "➜",
-    --                     package_uninstalled = "✗",
-    --                 },
-    --             },
-    --         })
-    --
-    --         mason_lspconfig.setup({
-    --             ensure_installed = {
-    --                 "lua_ls",
-    --                 "stylua"
-    --             },
-    --             automatic_enable = true
-    --         })
-    --
-    --         vim.lsp.config("texlab", {
-    --             cmd = { "texlab" },
-    --             filetypes = { "tex", "bib" },
-    --             settings = {
-    --                 texlab = {
-    --                     build = {
-    --                         onSave = false,
-    --                         executable = "latexmk",
-    --                         args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
-    --                     },
-    --                     chktex = { onOpenAndSave = true },
-    --                     -- This helps Texlab find your labels in complex documents
-    --                     auxDirectory = "./build/",
-    --                 },
-    --             },
-    --         })
-    --
-    --         vim.lsp.config("*", {
-    --             capabilites = require("blink.cmp").get_lsp_capabilities(),
-    --         })
-    --
-    --         vim.api.nvim_create_autocmd("LspAttach", {
-    --             callback = function(event)
-    --                 local map = function(keys, func, desc)
-    --                     vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-    --                 end
-    --                 map("<leader>df", vim.diagnostic.open_float, "[D]iagnostic [F]loat")
-    --                 map("<leader>bd", "<Cmd>Telescope diagnostics bufnr=0<CR>", "[B]uffer [D]Diagnostics")
-    --                 map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-    --                 map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-    --                 map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-    --                 map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-    --                 map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-    --                 map(
-    --                     "<leader>ws",
-    --                     require("telescope.builtin").lsp_dynamic_workspace_symbols,
-    --                     "[W]orkspace [S]ymbols"
-    --                 )
-    --                 map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-    --                 map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-    --                 map("K", vim.lsp.buf.hover, "Hover Documentation")
-    --                 map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-    --
-    --                 local client = vim.lsp.get_client_by_id(event.data.client_id)
-    --                 if client and client:supports_method("textDocument/documentHighlight", event.buf) then
-    --                     vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-    --                         buffer = event.buf,
-    --                         callback = vim.lsp.buf.document_highlight,
-    --                     })
-    --
-    --                     vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-    --                         buffer = event.buf,
-    --                         callback = vim.lsp.buf.clear_references,
-    --                     })
-    --
-    --                     vim.api.nvim_create_autocmd("LspDetach", {
-    --                         group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-    --                         callback = function(event2)
-    --                             vim.lsp.buf.clear_references()
-    --                             vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
-    --                         end,
-    --                     })
-    --                 end
-    --             end,
-    --         })
-    --     end,
-    -- },
-    -- { -- Autoformat
-    --     "stevearc/conform.nvim",
-    --     event = { "BufWritePre" },
-    --     cmd = { "ConformInfo" },
-    --     keys = {
-    --         {
-    --             "<leader>f",
-    --             function()
-    --                 require("conform").format({ async = true, lsp_format = "fallback" })
-    --             end,
-    --             mode = "",
-    --             desc = "[F]ormat buffer",
-    --         },
-    --     },
-    --     ---@module 'conform'
-    --     ---@type conform.setupOpts
-    --     opts = {
-    --         notify_on_error = false,
-    --         format_on_save = function(_)
-    --             return nil
-    --         end,
-    --     },
-    -- },
-    -- { -- Autocompletion
-    --     "saghen/blink.cmp",
-    --     event = "VimEnter",
-    --     version = "1.*",
-    --     dependencies = {
-    --         -- Snippet Engine
-    --         {
-    --             "L3MON4D3/LuaSnip",
-    --             version = "2.*",
-    --             build = (function()
-    --                 -- Build Step is needed for regex support in snippets.
-    --                 -- This step is not supported in many windows environments.
-    --                 -- Remove the below condition to re-enable on windows.
-    --                 if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-    --                     return
-    --                 end
-    --                 return "make install_jsregexp"
-    --             end)(),
-    --             dependencies = {
-    --                 {
-    --                     'rafamadriz/friendly-snippets',
-    --                     config = function()
-    --                         require('luasnip.loaders.from_vscode').lazy_load()
-    --                     end,
-    --                 },
-    --             },
-    --             opts = {},
-    --         },
-    --     },
-    --     ---@module 'blink.cmp'
-    --     ---@type blink.cmp.Config
-    --     opts = {
-    --         keymap = {
-    --             preset = "none",
-    --
-    --             ["<CR>"] = { "accept", "fallback" },
-    --             ["<C-e>"] = { "hide", "fallback" },
-    --
-    --             ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
-    --             ["<C-n>"] = { "show", "select_next", "fallback_to_mappings" },
-    --
-    --             ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-    --             ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-    --
-    --             ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
-    --         },
-    --         appearance = {
-    --             nerd_font_variant = "mono",
-    --         },
-    --         completion = {
-    --             documentation = { auto_show = true, auto_show_delay_ms = 0 },
-    --         },
-    --         sources = {
-    --             default = { "lsp", "path", "snippets" },
-    --         },
-    --         snippets = { preset = "luasnip" },
-    --         fuzzy = { implementation = "lua" },
-    --         signature = { enabled = true },
-    --     },
-    -- },
 }
